bf7930836b99acef76f3ccab4a84d95bf4e030c9
Ahmer Malik <ahmer9800@gmail.com>
2017-11-06 17:55:52 -0600

    add files

---
+++ interviewchallengewk8.js
@@ -0,0 +1,14 @@
+
+
+function zero() {
+emptyArray = ''
+listOfNumbers = [1,2,-1,-2,10,-9,-5,7,1,-1,-10]
+
+for (number in listOfNumbers){
+    if number + number = 0
+        array.append[emptyArray];
+}
+    console.log(listOfNumbers[6]);
+};
+
+zero()
